e9ef707a41e6c111f6852b1f2ddb4b5b0cfb09f4
universocelular <universocelularargentina@gmail.com>
2025-07-24 20:22:45 -0300

    Corregir definitivamente el tipo en ServiceDetailPage

--- src/app/admin/services/[id]/page.tsx
+++ src/app/admin/services/[id]/page.tsx
@@ -2,12 +2,8 @@ import { getServiceById } from "@/lib/actions/services";
 import { notFound } from "next/navigation";
 import { ServiceDetailClient } from "@/components/admin/service-detail-client";
 
-interface ServiceDetailPageProps {
-  params: { id: string };
-}
-
 // This is now a Server Component responsible for data fetching.
-export default async function ServiceDetailPage({ params }: ServiceDetailPageProps) {
+export default async function ServiceDetailPage({ params }: { params: { id: string } }) {
   const serviceId = params.id;
   const service = await getServiceById(serviceId);
 
